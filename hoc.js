b024bed7e04f059f1ed47e638d3b4e92e3a52deb
Sinh Cute <baosinh2k@gmail.com>
2022-03-09 20:57:52 +0700

    hh

---
+++ hoc.js
@@ -0,0 +1,14 @@
+//alert("da tao trang web");
+//var b = prompt("Nhap vao gia tri A");
+//console.log("gia tri bien b la: ", b);
+//console.log("thong bao");
+//console.log("gia tri la:", 1);
+var x, y, z;
+//x = 5;
+//y = 6;
+x = prompt("nhap vao gia tri x");
+y = prompt("nhap vao gia tri y");
+z = parseInt(x) + parseInt(y);
+
+//alert("gia tri z la" + z);
+console.log("gia tri z la " + z);
